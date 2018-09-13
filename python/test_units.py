5d28c1e66b2a26751f04d5eee507326fe994118e
Nikolas Schnellbächer <khx0@posteo.net>
2018-09-13 15:16:19 +0200

    added gitignore

--- python/test_units.py
+++ python/test_units.py
@@ -5,6 +5,8 @@
 # contact: khx0@posteo.net
 # date: 2018-09-13
 # file: test_units.py
+# tested with python 2.7.15
+# tested with python 3.7.0
 ##########################################################################################
 
 import sys
@@ -35,6 +37,54 @@ class Units_test(unittest.TestCase):
         concentrationValue = 2.0 * MOLAR_TO_MICROMETER
         
         self.assertTrue(np.isclose(referenceValue, concentrationValue))
+        
+        return None
+        
+    def test_Units_02(self):
+        
+        # 3D -> 2D affinity conversion factor
+        conv_3D_to_2D_affinity = 1.2e-3 # in um
+        
+        # 3D equilibrium affinity
+        KDB = 0.1e-9 # in Molar (M)
+        
+        KDX = conv_3D_to_2D_affinity * KDB * MOLAR_TO_MICROMETER
+        
+        KDX_reference = 0.722656891e-4 # in 1/ (um)^2
+                        
+        self.assertTrue(np.isclose(KDX, KDX_reference))
+        
+        return None
+
+    def test_Units_03(self):
+        
+        # 3D -> 2D affinity conversion factor
+        conv_3D_to_2D_affinity = 1.2e-3 # in um
+        
+        # 3D equilibrium affinity
+        KDB = 100e-9 # in Molar (M)
+        
+        KDX = conv_3D_to_2D_affinity * KDB * MOLAR_TO_MICROMETER
+        
+        KDX_reference = 0.0722656891 # in 1/ (um)^2
+                        
+        self.assertTrue(np.isclose(KDX, KDX_reference))
+        
+        return None
+
+    def test_Units_04(self):
+        
+        # 3D -> 2D affinity conversion factor
+        conv_3D_to_2D_affinity = 1.2e-3 # in um
+        
+        # 3D equilibrium affinity
+        KDB = 1.0e-6 # in Molar (M)
+        
+        KDX = conv_3D_to_2D_affinity * KDB * MOLAR_TO_MICROMETER
+        
+        KDX_reference = 0.722656891 # in 1/ (um)^2
+                        
+        self.assertTrue(np.isclose(KDX, KDX_reference))
         
         return None
 
